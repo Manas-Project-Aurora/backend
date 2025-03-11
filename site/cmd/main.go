c804184ec580184e46a4bcb5fa5dfb52d02b48c8
alexeySTAV <wehheh32@gmail.com>
2025-03-11 15:31:26 +0600

    Add flag parsing

--- site/cmd/main.go
+++ site/cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/Manas-Project-Aurora/gavna/site/internal/config"
 	"github.com/Manas-Project-Aurora/gavna/site/internal/server"
 )
 
 func main() {
-	server.NewServer().SetPort(8080).SetDBConfig("dbconfig.yaml").Run()
+	cfg := config.ParseFlags()
+	server.NewServer().SetPort(cfg.Port).SetDBConfig(cfg.DBConfigPath).Run()
 }
