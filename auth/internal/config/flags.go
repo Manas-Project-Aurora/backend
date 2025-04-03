271af7293b300413a2e1398ebeb2003b8476b8ae
alexeySTAV <wehheh32@gmail.com>
2025-04-03 19:51:23 +0600

    Add CORS to auth

--- auth/internal/config/flags.go
+++ auth/internal/config/flags.go
@@ -9,20 +9,25 @@ type CLIConfig struct {
 	Port         uint
 	DBConfigPath string
 	BasePath     string
+	Domain       string
 }
 
 func ParseFlags() CLIConfig {
 	port := pflag.UintP("port", "p", 8080, "Port for the server")
 	path := pflag.StringP("dbpath", "d", "dbconfig.yaml", "Set dbConfig.yaml path")
 	base := pflag.StringP("basepath", "b", "", "Set base path")
+	domain := pflag.StringP("url", "u", "avtomoykabot.store", "Set site's url")
+
 	pflag.Parse()
 
-	// Убираем завершающий слеш, если есть
-	basePath := strings.TrimSuffix(*base, "/")
+	*base = strings.TrimSuffix(*base, "/")
+	*domain = strings.TrimPrefix(*domain, "https://")
+	*domain = strings.TrimPrefix(*domain, "http://")
 
 	return CLIConfig{
 		Port:         *port,
 		DBConfigPath: *path,
-		BasePath:     basePath, // *base
+		BasePath:     *base,
+		Domain:       *domain,
 	}
 }
--- auth/internal/server/server.go
+++ auth/internal/server/server.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"github.com/Manas-Project-Aurora/gavna/auth/internal/config"
 	"github.com/Manas-Project-Aurora/gavna/auth/internal/db"
+	"github.com/gin-contrib/cors"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -18,10 +20,11 @@ type Server struct {
 	port       uint
 	dbYamlPath string
 	basePath   string
+	Domain     string
 }
 
 func NewServer(c config.CLIConfig) *Server {
-	return &Server{port: c.Port, dbYamlPath: c.DBConfigPath, basePath: c.BasePath}
+	return &Server{port: c.Port, dbYamlPath: c.DBConfigPath, basePath: c.BasePath, Domain: c.Domain}
 }
 
 func (s *Server) SetPort(port uint) *Server {
@@ -38,6 +41,11 @@ func (s *Server) SetBasePath(basePath string) *Server {
 	return s
 }
 
+func (s *Server) SetSiteDomain(domain string) *Server {
+	s.Domain = domain
+	return s
+}
+
 func (s *Server) Run() {
 	errChan := make(chan error, 1)
 	sigChan := make(chan os.Signal, 1)
@@ -48,6 +56,17 @@ func (s *Server) Run() {
 		errChan <- fmt.Errorf("Базе пизда: %v", err)
 	}
 	router := SetupRoutes(db, s.basePath)
+	router.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			return strings.HasSuffix(origin, "."+s.Domain) || origin == "https://"+s.Domain
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", s.port),
 		Handler: router,
